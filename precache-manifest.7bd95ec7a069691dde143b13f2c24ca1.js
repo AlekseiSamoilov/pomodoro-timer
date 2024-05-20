c4eea01d1540391615feab3109d70a5bd6d349f2
Aleksei Samoilov <lek-z@yandex.ru>
2024-05-20 14:30:43 +0200

    Updates

---
+++ precache-manifest.7bd95ec7a069691dde143b13f2c24ca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ec4469f7d4b36be9d7c800e6cadc1c",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "fa8b4ec08c085905f4c1",
+    "url": "/pomodoro-timer/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "c7d43224a927194e55c9",
+    "url": "/pomodoro-timer/static/css/main.7b1f29ef.chunk.css"
+  },
+  {
+    "revision": "fa8b4ec08c085905f4c1",
+    "url": "/pomodoro-timer/static/js/2.55edb3cd.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/pomodoro-timer/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "c7d43224a927194e55c9",
+    "url": "/pomodoro-timer/static/js/main.83f42569.chunk.js"
+  },
+  {
+    "revision": "ae2771cac62234293722",
+    "url": "/pomodoro-timer/static/js/runtime~main.b81a2bb4.js"
+  }
+]);
